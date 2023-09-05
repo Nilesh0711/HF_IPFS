bf0e172df067749e4ed239e341272c4e37d7866c
Nilesh Gupta <nilgupta2002@gmail.com>
2023-09-05 16:14:10 +0530

    ipfs_test

---
+++ app.js
@@ -0,0 +1,84 @@
+// "use strict";
+// import { create } from "ipfs-http-client";
+const ipfs = require("ipfs-http-client");
+const fs = require('fs');
+
+const createClient = async () => {
+  const _ipfs = ipfs.create({
+    host: "localhost",
+    port: '5001',
+    protocol: "http",
+  });
+  return _ipfs;
+};
+
+const saveText = async () => {
+  let ipfs = await createClient();
+  let result = await ipfs.add("hello world my name is nilesh gupta i learning ipfs for my current project orphanage mangament using hyperledger fabric and ipfs i love coding and playing games like counter strike counter strike gobal offensive");
+  console.log(result);
+};
+
+const saveImage = async () => {
+  let ipfs = await createClient();
+  let data = fs.readFileSync("./firewatch.jpg")
+  data = Buffer.from(data)
+  let options = {
+    wrapWithDirectory:false,
+    progress:(prog)=>console.log(`Saved : ${prog}`)
+  }
+  let result = await ipfs.add(data, options)
+  console.log(result);
+};
+
+const savePdf = async () => {
+  let ipfs = await createClient();
+  let data = fs.readFileSync("./blockdiag.pdf")
+  // data = Buffer.from(data)
+  let options = {
+    wrapWithDirectory:false,
+    progress:(prog)=>console.log(`Saved : ${prog}`)
+  }
+  let result = await ipfs.add(data, options)
+  console.log(result);
+};
+
+const readText = async (cid) => {
+  let ipfs = await createClient();
+  let result = ipfs.cat(cid)
+  for await (const itr of result){
+    console.log(Buffer.from(itr).toString());
+  }
+}
+
+const readImage = async (cid) => {
+  let ipfs = await createClient();
+  let result = ipfs.cat(cid)
+  for await (const itr of result){
+    console.log(Buffer.from(itr).toString());
+  }
+}
+
+const readPdf = async (cid) => {
+  let ipfs = await createClient();
+  let result = ipfs.cat(cid)
+  for await (const itr of result){
+    console.log(Buffer.from(itr).toString());
+  }
+}
+
+function main() {
+  console.log("main function is running");
+  // saveText();
+  // saveImage()
+  savePdf()
+  // let dataText = "QmX1bWA7gTDYkUdeX64B9jfbPP4g1xMDPSwPVUfEWVTZfp"
+  // let dataImage = "QmUCdQPSptmkRN9k7EduPbb4WNUin5JLmx3oR84Yv81eeH"
+  // let dataImage = "QmYwF3PeJNvPsJYAyNC6TbBgQ6Jnde3kmEPpdvYBY2EdGK"
+  // let dataPdf = "QmQbMNFZjuSsypQTXw3a2dxf1j2pv3hKF42foDa3Emy7P7"
+  // readText(dataText)
+  // readImage(dataImage)
+  // readPdf(dataPdf)
+  // QmVhsmBt1inpq6Hp99eqYcSfcL6cTBQB6j5SiM4Q9rbPEF
+}
+main();
+
---
+++ node.js
@@ -0,0 +1,5 @@
+const generator = require('js-ipfs-swarm-key-gen');
+ 
+// generator().then(() => console.log('done'))
+// or
+generator('/home/nilesh/swarm.key').then(() => console.log('done'))
